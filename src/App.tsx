24a29a3bd29f0da3d7196f998cec97a9a7c02148
ws456999 <ruiru5921@sina.com>
2018-03-14 15:20:38 +0800

    :tada:

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import './App.css'
+
+const logo = require('./logo.svg')
+
+class App extends React.Component<{}, {}> {
+  @f()
+  method () {
+    alert(123)
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+function f () {
+  // console.log('f(): evaluated')
+  return function (
+    target: object,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    // console.log('f(): called')
+  }
+}
+
+export default App
