78c430b3e5de6b957c53a317bf4afc9879a3dc63
ws456999 <ruiru5921@sina.com>
2018-03-14 21:45:00 +0800

    test

--- docker/run.sh
+++ docker/run.sh
@@ -4,11 +4,9 @@ sudo rm -rf ./build
 
 sudo cp -r ./../build ./
 
-docker containter stop react-ts
+docker rm -f $(docker ps -a -q)
 
-docker containter rm react-ts
-
-docker rmi docker_ts
+docker-compose build
 
 docker-compose up -d
 
